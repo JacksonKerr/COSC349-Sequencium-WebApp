977b2d6cb1847d6e46ff6e4c936a18016e7b79ba
jkerr <jacksonkerr2@gmail.com>
2020-08-22 16:16:33 +1200

    Fixed errors with creating vms

--- webserver/webserver.sh
+++ webserver/webserver.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
-    apt-get update
-    apt-get install -y apache2 php libapache2-mod-php php-mysql
-        
-    # Change VM's webserver's configuration to use shared folder.
-    # (Look inside test-website.conf for specifics.)
-    cp /vagrant/seq_site.conf /etc/apache2/sites-available/
-    # activate our website configuration ...
-    a2ensite seq_site
-    # ... and disable the default website provided with Apache
-    a2dissite 000-default
-    # Reload the webserver configuration, to pick up our changes
-    service apache2 reload
-SHELL
+apt-get update
+apt-get install -y apache2 php libapache2-mod-php php-mysql
+    
+# Change VM's webserver's configuration to use shared folder.
+# (Look inside test-website.conf for specifics.)
+cp /vagrant/seq_site.conf /etc/apache2/sites-available/
+# activate our website configuration ...
+a2ensite seq_site
+# ... and disable the default website provided with Apache
+a2dissite 000-default
+# Reload the webserver configuration, to pick up our changes
+service apache2 reload
