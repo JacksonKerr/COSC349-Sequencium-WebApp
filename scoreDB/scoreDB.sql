86aaa225cca184b0b13d612e9d457fbc4f1fd78c
Megan Wood <meggywood@gmail.com>
2020-08-31 13:33:41 +1200

    scoreDB fomratted as a table in webserver.

--- scoreDB/scoreDB.sql
+++ scoreDB/scoreDB.sql
@@ -1,9 +1,9 @@
 CREATE TABLE scores (
        username varchar(50),
        score int,
-       agent_score int,
        PRIMARY KEY (username)
 );
 
-INSERT INTO scores VALUES ('Megan', 40, 12);
-INSERT INTO scores VALUES ('Jackson', 6, 20);
+INSERT INTO scores VALUES ('Megan', 40);
+INSERT INTO scores VALUES ('Jackson', 6);
+INSERT INTO scores VALUES ('SeqChampion', 70);
