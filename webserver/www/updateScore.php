65494b7edcde84e9054d2763612d5a960290ba34
Megan Wood <meggywood@gmail.com>
2020-08-31 16:45:34 +1200

    updtaescore.php edited so old score is replaced when same user plays and when new score is larger than previously held score

--- webserver/www/updateScore.php
+++ webserver/www/updateScore.php
@@ -1,6 +1,6 @@
 <?php
 $db_host   = '192.168.5.4';
-$db_name   = 'scoreDB';
+$db_name   = 'scoreHold';
 $db_user   = 'webuser';
 $db_passwd = 'insecure_db_pw';
 $board_name = $_GET['name'];
@@ -11,8 +11,8 @@ echo $board_score;
 
 $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 
-echo "Hello";
 $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-$q = $pdo->query("INSERT INTO score VALUES ('$board_name', $board_score)");
-echo "done";
+$pdo->query("DELETE FROM scores WHERE username = '$board_name' AND score < $board_score");
+$q = $pdo->query("INSERT INTO scores VALUES ('$board_name', $board_score)");
+echo $q;
 ?>
